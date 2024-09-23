36bd1be0970fc3821622ce9a2daa70821a8b9c37
Amit Netanel <amitn@papaya.com>
2024-09-23 12:55:08 +0300

    stateful

--- Assets/Scripts/InputController/InputController.cs
+++ Assets/Scripts/InputController/InputController.cs
@@ -7,9 +7,10 @@ namespace InputController
     public partial class InputController : MonoBehaviour
     {
         [SerializeField] private Camera _controlCamera;
-        [SerializeField] private Actor _actor;
         private IAudioManager _audioManager;
 
+        private Actor _currentlySelectedActor;
+        
         [Inject]
         public void Construct(IAudioManager audioManager)
         {
